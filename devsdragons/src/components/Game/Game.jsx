5e906eb04d9a91868b4870e3902f201519024072
AnimishTemple <animishtenneti@gmail.com>
2024-10-17 21:03:39 -0400

    Basic world and body with arguments are imported and initialized as animation is intended

--- devsdragons/src/components/Game/Game.jsx
+++ devsdragons/src/components/Game/Game.jsx
@@ -1,10 +1,14 @@
-import { Loop, Stage } from 'react-game-kit';
+import { Loop, Stage, World, Body } from 'react-game-kit';
 
 const Game = () => {
   return (
     <Loop>
         <Stage>
-            
+            <World>
+                <Body args={[0,0,75,75]} ref={ (b) => this.body = b.body }>
+
+                </Body>
+            </World>
         </Stage>
     </Loop>
   )
