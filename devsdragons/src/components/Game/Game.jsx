176c4ba3f79bbb36239b09c09fcae064663fe377
AnimishTemple <animishtenneti@gmail.com>
2024-10-17 21:02:15 -0400

    imported loop and stage into the game component

---
+++ devsdragons/src/components/Game/Game.jsx
@@ -0,0 +1,13 @@
+import { Loop, Stage } from 'react-game-kit';
+
+const Game = () => {
+  return (
+    <Loop>
+        <Stage>
+            
+        </Stage>
+    </Loop>
+  )
+}
+
+export default Game
